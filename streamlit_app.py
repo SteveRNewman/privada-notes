4fa30eb9ee0c1a1b92d12426b2453bb94fc5a330
SteveNewman <stevenewmanphotography@gmail.com>
2022-01-04 11:22:36 -0500

    V1.1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,31 @@
 import streamlit as st
-import os
+action = st.radio('Action', ['Submit Notes', "Search Notes"])
+if action == "Submit Notes":
+    with st.form('Form1'):
+        st.selectbox('Subscription Type', ['Privada Box', 'Farm Rolled', "Brian's Box",'Test Blends'], key=1)
+        st.selectbox('Year', ['2017', '2018', "2019",'2020', '2021', '2022'], key=2)
+        st.selectbox('Month', ['January', 'February', "March",'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November','December'], key=3)
+        st.text_input(label='Cigar One')
+        st.text_input(label='Cigar Two')
+        st.text_input(label='Cigar Three')
+        st.text_input(label='Cigar Four')
+        uploaded_files = st.file_uploader("Choose Notes Images", accept_multiple_files=True)
+        for uploaded_file in uploaded_files:
+            bytes_data = uploaded_file.read()
+            st.write("filename:", uploaded_file.name)
+            st.write(bytes_data)
+        submit_button = st.form_submit_button(label='Submit')
 
-with st.form('Form1'):
-    st.selectbox('Subscription Type', ['Privada Box', 'Farm Rolled', "Brian's Box",'Test Blends'], key=1)
-    st.selectbox('Year', ['2017', '2018', "2019",'2020', '2021', '2022'], key=2)
-    st.selectbox('Month', ['January', 'February', "March",'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November','December'], key=3)
-    st.text_input(label='Cigar One')
-    st.text_input(label='Cigar Two')
-    st.text_input(label='Cigar Three')
-    st.text_input(label='Cigar Four')
-    uploaded_files = st.file_uploader("Choose Notes Images", accept_multiple_files=True)
-    for uploaded_file in uploaded_files:
-        bytes_data = uploaded_file.read()
-        st.write("filename:", uploaded_file.name)
-        st.write(bytes_data)
-    submit_button = st.form_submit_button(label='Submit')
-
+if action == "Search Notes":
+    st.selectbox ("Cigar", ['Cigar 1','Cigar 2','Cigar 3', 'Cigar 4'],key=4)
+    with open("flower.png", "rb") as file:
+     btn = st.download_button(
+             label="Download image",
+             data=file,
+             file_name="flower.png",
+             mime="image/png"
+           )
+    st.image('flower.png')
 # def save_uploadedfile(uploadedfile):
 #     with open(os.path.join('Data', uploadedfile.name), 'wb') as f:
 #      f.write(uploadedfile.getbuffer())
