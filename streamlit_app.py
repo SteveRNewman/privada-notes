ae2a7fdacdc576da88e220ee908bf7cfa9cb227e
SteveNewman <stevenewmanphotography@gmail.com>
2022-01-04 10:51:05 -0500

    V1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,29 @@
 import streamlit as st
+import os
 
-st.header('Hello 🌎!')
-if st.button('Balloons?'):
-    st.balloons()
+with st.form('Form1'):
+    st.selectbox('Subscription Type', ['Privada Box', 'Farm Rolled', "Brian's Box",'Test Blends'], key=1)
+    st.selectbox('Year', ['2017', '2018', "2019",'2020', '2021', '2022'], key=2)
+    st.selectbox('Month', ['January', 'February', "March",'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November','December'], key=3)
+    st.text_input(label='Cigar One')
+    st.text_input(label='Cigar Two')
+    st.text_input(label='Cigar Three')
+    st.text_input(label='Cigar Four')
+    uploaded_files = st.file_uploader("Choose Notes Images", accept_multiple_files=True)
+    for uploaded_file in uploaded_files:
+        bytes_data = uploaded_file.read()
+        st.write("filename:", uploaded_file.name)
+        st.write(bytes_data)
+    submit_button = st.form_submit_button(label='Submit')
+
+# def save_uploadedfile(uploadedfile):
+#     with open(os.path.join('Data', uploadedfile.name), 'wb') as f:
+#      f.write(uploadedfile.getbuffer())
+#     return st.success('Saved File:{} to Data'.format(uploadedfile.name))
+
+# st.title(' PDF File upload')
+# st.text(' A simple way to upload files directly into a directory')
+# uploadedfiles = st.file_uploader('Upload PDF', type=["pdf"], accept_multiple_files=True)
+# for file in uploadedfiles:
+#     if uploadedfiles is not None:
+#         save_uploadedfile(file) 
