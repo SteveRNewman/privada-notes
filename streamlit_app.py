ad495d9d2c3bfd4e2defbbdcf3dab5a433d81cec
SteveNewman <stevenewmanphotography@gmail.com>
2022-01-04 21:56:25 -0500

    data

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,33 +26,42 @@ import pandas as pd
 import numpy as np
 import os
 from PIL import Image
+import s3fs
 
-action = st.radio('Action', ['Submit Notes', "Search Notes","View Archive"])
+
+df = pd.DataFrame(columns=('Subscription', "Year", 'Month', 'Cigar 1', "Cigar 2", 'Cigar 3', 'Cigar 4'))#[['Privada Box','2021','December','Cigar A','Cigar B','Cigar C', 'Cigar D','flowers.png','flowers2.png']],
+    #columns=('Subscription', "Year", 'Month', 'Cigar 1', "Cigar 2", 'Cigar 3', 'Cigar 4', 'Notes Front', 'Notes Back'))
+# my_dict1 = {"Cigar 1":[],"Cigar 2":[],"Cigar 3":[],"Cigar 4":[]}
+# my_dict = {"Subscription":[],"Year":[],"Month":[],"Cigars": my_dict1,"Notes Front":[],"Notes Back":[]}
+
+# my_dict["Cigars"] = my_dict1
+action = st.sidebar.radio('Action', ['Submit Notes', "Search Notes"])
 if action == "Submit Notes":
-    with st.form('Form1'):
-        st.selectbox('Subscription Type', ['Privada Box', 'Farm Rolled', "Brian's Box",'Test Blends'], key=1)
-        st.selectbox('Year', ['2017', '2018', "2019",'2020', '2021', '2022'], key=2)
-        st.selectbox('Month', ['January', 'February', "March",'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November','December'], key=3)
-        st.text_input(label='Cigar One')
-        st.text_input(label='Cigar Two')
-        st.text_input(label='Cigar Three')
-        st.text_input(label='Cigar Four')
-        uploaded_file_f = st.file_uploader("Choose Notes Front")
-        if uploaded_file_f is not None:
-        # To read file as bytes:
-            bytes_data = uploaded_file_f.getvalue()
-            st.write(bytes_data)
-        uploaded_file_b = st.file_uploader("Choose Notes Back")
-        if uploaded_file_b is not None:
-        # To read file as bytes:
-            bytes_data = uploaded_file_b.getvalue()
-            st.write(bytes_data) 
-        # uploaded_files = st.file_uploader("Choose Notes Front", accept_multiple_files=False)
-        # for uploaded_file in uploaded_files:
-        #     bytes_data = uploaded_file.read()
-        #     st.write("filename:", uploaded_file.name)
-        #     st.write(bytes_data)
-        submit_button = st.form_submit_button(label='Submit')
+    with st.sidebar.form(key ='Form1'):
+        sub = st.selectbox('Subscription Type', ['Privada Box', 'Farm Rolled', "Brian's Box",'Test Blends'], key=1)
+        year = st.selectbox('Year', ['2017', '2018', "2019",'2020', '2021', '2022'], key=2)
+        month = st.selectbox('Month', ['January', 'February', "March",'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November','December'], key=3)
+        c1 = st.text_input(label='Cigar One')
+        c2 = st.text_input(label='Cigar Two')
+        c3 = st.text_input(label='Cigar Three')
+        c4 = st.text_input(label='Cigar Four')
+        f_uploaded_file = st.file_uploader("Choose Notes Front")
+        if f_uploaded_file is not None:
+            image = Image.open(f_uploaded_file)
+        b_uploaded_file = st.file_uploader("Choose Notes Back")
+        if b_uploaded_file is not None:
+            image = Image.open(b_uploaded_file)
+
+        submit = st.form_submit_button(label='Submit')
+        if submit:
+            df2 = pd.DataFrame([[sub,year,month,c1,c2,c3,c4,c4,c4]], columns=('Subscription', "Year", 'Month', 'Cigar 1', "Cigar 2", 'Cigar 3', 'Cigar 4','Front','Back'))#,uff,ufb])
+            bytes_to_write = df2.to_csv(None).encode()
+            fs = s3fs.S3FileSystem(key=key, secret=secret)
+            with fs.open('s3://privada-df/privada_data.csv', 'wb') as f:
+                f.write(bytes_to_write)
+
+    st.write("Archive of notes already submitted can be sorted by clicking on the column name.")
+    st.dataframe(df2, width=2000)
 
 if action == "Search Notes":
     search = st.selectbox ("Cigar", ['Cigar 1','Cigar 2','Cigar 3', 'Cigar 4'],key=4)
@@ -94,15 +103,6 @@ if action == "Search Notes":
             st.image('flower4.png')
 
 
-if action == "View Archive":
-    df = pd.DataFrame([['Privada Box','2021','December','Cigar A','Cigar B','Cigar C', 'Cigar D','/flowers.png','flowers2.png'],
-    ['Privada Box','2021','December','Cigar A','Cigar B','Cigar C', 'Cigar D','/flowers.png','flowers2.png'],
-    ['Privada Box','2021','December','Cigar A','Cigar B','Cigar C', 'Cigar D','/flowers.png','flowers2.png'],
-    ['Privada Box','2021','December','Cigar A','Cigar B','Cigar C', 'Cigar D','/flowers.png','flowers2.png'],
-    ['Privada Box','2021','December','Cigar A','Cigar B','Cigar C', 'Cigar D','/flowers.png','flowers2.png']],
-    columns=('Subscription', "Year", 'Month', 'Cigar 1', "Cigar 2", 'Cigar 3', 'Cigar 4', 'Notes Front', 'Notes Back'))
-
-    st.dataframe(df)
 # # def save_uploadedfile(uploadedfile):
 # #     with open(os.path.join('Data', uploadedfile.name), 'wb') as f:
 # #      f.write(uploadedfile.getbuffer())
